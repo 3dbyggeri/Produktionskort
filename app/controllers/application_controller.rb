1b7bf2deb65a29f2d6634f966c28cf60eac24e4a
Thomas Watson Steen <w@tson.dk>
2010-10-14 22:27:29 +0200

    Added HTTP basic auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,18 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
+  before_filter :authenticate
+
   private
+
+  def authenticate
+    authenticate_or_request_with_http_basic('Produktionskort') do |username, password|
+      md5_of_username = Digest::MD5.hexdigest(username)
+      md5_of_password = Digest::MD5.hexdigest(password)
+      md5_of_username == '21232f297a57a5a743894a0e4a801fc3' && md5_of_password == '28b17a9a76659fae8b968a71510d001e'
+    end
+    warden.custom_failure! if performed?
+  end
 
   def process_attachments(base_key, attachment_keys)
     attachment_keys.each do |attachment_key|
